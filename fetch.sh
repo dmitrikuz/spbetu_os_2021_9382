d9ddd7e5b2fccc57106b205f4fa9cc5af8e9a6df
Mikhail Efremov <jakutenshi@gmail.com>
2021-05-12 18:20:59 +0300

    -Dokukin from fetch

--- fetch.sh
+++ fetch.sh
@@ -17,5 +17,5 @@ git fetch MilanaBalaeva   # Балаева
 git fetch sergeyyurev     # Юрьев
 git fetch Ryzhikh-Roman   # Рыжих
 git fetch NereusOcean     # Иерусалимов
-git fetch tehril17        # Докукин
+#git fetch tehril17        # Докукин
 #git fetch                # 
