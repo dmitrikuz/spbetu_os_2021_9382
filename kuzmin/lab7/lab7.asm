5ed5e399524ccebff5251be48c71c7f34db0e3f0
dmitrikuz <54910797+dmitrikuz@users.noreply.github.com>
2021-05-14 09:43:44 +0300

    lab7: update

--- kuzmin/lab7/lab7.asm
+++ kuzmin/lab7/lab7.asm
@@ -7,6 +7,7 @@ overlay_name1 db 'overlay1.ovl', 0
 overlay_name2 db 'overlay2.ovl', 0
 overlay_name_addr dw 0
 setflag db 1
+path db 50 dup(0)
 mem_error db 'Memory error. Code    ', 13, 10, '$'
 load_error db 'Overlay loading error. Code   ' , 13, 10, '$'
 size_detect_error db 'Unable to detect size of overlay', 13, 10, '$'
@@ -16,6 +17,7 @@ dta db 43 dup(0)
 overlay_entry_point   dd  0
 keep_sp dw 0
 keep_ss dw 0
+psp dw 0
 DATA ENDS
 CODE SEGMENT
      .386
@@ -98,11 +100,11 @@ FREE_MEMORY PROC NEAR
 			push si
 			push dx
 			mov bx, offset end_point
-			mov ax,es
-			sub bx,ax
-			mov ax,bx
-			shr bx,4
-			add bx, 1
+			mov ax, cs
+			add bx, ax
+			shr bx,4h
+			add bx, 1Fh
+
 			mov ah,4ah
 			int 21h
 			jc  error_message2
@@ -127,7 +129,7 @@ SET_OVERLAY PROC NEAR
 			int 21h
 			
 			mov ah,4Eh
-			mov dx, overlay_name_addr
+			mov dx, offset path
 			mov cx,0
 			int 21h
 			jc error_message1
@@ -155,14 +157,15 @@ SET_OVERLAY PROC NEAR
 			ret
 SET_OVERLAY ENDP
 RUN_OVERLAY PROC NEAR
+			
 			mov     ax,ds          
 			mov     es,ax
 	 
-			mov     dx, overlay_name_addr
+			mov     dx, offset path
 			mov     bx,offset epb  
 			mov     ax,4b03h       
 			int     21h    
-			
+			jc load_error_msg
 			
 			mov dx, offset overlay_loaded
 			call WRITE_MSG
@@ -173,6 +176,18 @@ RUN_OVERLAY PROC NEAR
 			mov es, epb
 			mov ah, 49h
 			int 21h
+			jmp exit_run
+			load_error_msg:
+			push si
+			push dx
+			mov si, offset load_error
+			add si, 30
+			call BYTE_TO_DEC
+			mov dx, offset load_error
+			call WRITE_MSG
+			pop dx
+			pop si
+			exit_run:
 			ret
 RUN_OVERLAY ENDP
 LOAD_OVERLAY PROC NEAR
@@ -203,15 +218,81 @@ LOAD_OVERLAY PROC NEAR
 			pop ax
 			ret
 LOAD_OVERLAY ENDP
+GET_PATH_TO_FILE proc near
 
+				push di
+				push si
+				push ax
+				push bx
+				push cx
+				push dx
+				push es
+
+
+				mov ax, psp
+				mov es, ax
+				mov es, es:[2ch]
+				mov bx, 0
+
+			continue:
+				inc bx
+				cmp byte ptr es:[bx-1], 0
+				jne continue
+
+				cmp byte ptr es:[bx+1], 0
+				jne continue
+
+				add bx, 2
+				mov di, 0
+
+			check:
+				mov dl, es:[bx]
+				mov byte ptr [path+di], dl
+				add di, 1
+				add bx, 1
+				cmp dl, 0
+				je end_check
+				cmp dl, '\'
+				jne check
+				mov cx, di
+				jmp check
+			end_check:
+				mov di, cx
+				mov si, overlay_name_addr
+
+			write_end:
+				mov dl, byte ptr [si]
+				mov byte ptr [path+di], dl
+				inc di
+				inc si
+				cmp dl, 0
+				jne write_end
+				
+				pop es
+				pop dx
+				pop cx
+				pop bx
+				pop ax
+				pop si
+				pop di
+				ret
+GET_PATH_TO_FILE endp
 MAIN 		PROC FAR
 
 			mov ax,DATA
 			mov ds,ax
-			call FREE_MEMORY		
+			mov psp, es
+			xor ax, ax
+			call FREE_MEMORY	
+			
+			mov path, offset overlay_name1
 			mov overlay_name_addr, offset overlay_name1
+			call GET_PATH_TO_FILE
 			call LOAD_OVERLAY
+			
+			mov path, offset overlay_name2
 			mov overlay_name_addr, offset overlay_name2
+			call GET_PATH_TO_FILE
 			call LOAD_OVERLAY
 			
 			mov ah, 4ch
